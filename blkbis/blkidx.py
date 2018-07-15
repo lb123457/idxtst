811dab03949a6a4c14fe7e93d35320b14b8c0e19
ludovicbreger <lb123457@gmail.com>
2018-07-14 20:31:36 -0700

    Added skeleton classes.

--- blkbis/blkidx.py
+++ blkbis/blkidx.py
@@ -1,1 +1,49 @@
-$Id$
+'''
+
+This is a module to create and manipulate indices.
+
+'''
+
+
+import os
+import sys
+import logging
+
+
+
+def __init__(self):
+    print('Main init at the top')
+
+
+class BlkIdx:
+
+
+    def __init__(self, name, **kwargs):
+        self.name = name
+
+        if 'description' in kwargs:
+            self.description = kwargs['description']
+
+
+    def print(self):
+        print("Index name = " + self.name)
+        if hasattr(self, 'description'):
+            print("Index description = " + self.description)
+
+
+
+class BlkEquityIdx(BlkIdx):
+
+
+    def __init__(self, name, **kwargs):
+        BlkIdx.__init__(self, name, **kwargs)
+
+
+
+if __name__ == "__main__":
+
+    idx = BlkIdx('Top level index', description='This is a dummy index')
+    idx.print()
+
+    idx = BlkEquityIdx('Equity index', description='This is a dummy equity index')
+    idx.print()
