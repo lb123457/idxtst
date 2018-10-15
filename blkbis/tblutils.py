4ec764b98322b7c106979bdd297e6353baf5e8a3
ludovicbreger <lb123457@gmail.com>
2018-10-14 19:52:14 -0700

    More changes.

--- blkbis/tblutils.py
+++ blkbis/tblutils.py
@@ -58,6 +58,104 @@ def parseArguments():
     return vars( parser.parse_args() )
 
 
+'''
+Creates a schema from a pandas dataframe.
+'''
+
+
+from pandas.api.types import is_string_dtype
+from pandas.api.types import is_numeric_dtype
+from pandas.api.types import is_integer_dtype
+from pandas.api.types import is_float_dtype
+from pandas.api.types import is_datetime64_dtype
+
+import math
+
+def valueIsMissing(v):
+    if math.isnan(v):
+        return True
+    else:
+        return False
+
+
+def df2schema(df, filename):
+
+    try:
+        extract = Extract( filename )
+
+        if ( not extract.hasTable( 'Extract' ) ):
+            schema = TableDefinition()
+            schema.setDefaultCollation( Collation.EN_GB )
+
+            for c in df.columns:
+                if is_string_dtype(df[c]):
+                    schema.addColumn(c, Type.CHAR_STRING)
+                    df[c].fillna('', inplace=True)
+                elif is_datetime64_dtype(df[c]):
+                    schema.addColumn(c, Type.DATETIME)
+                elif is_integer_dtype(df[c]):
+                    schema.addColumn(c, Type.INTEGER)
+                    df[c].fillna(0, inplace=True)
+                elif is_float_dtype(df[c]):
+                    schema.addColumn(c, Type.DOUBLE)
+                    df[c].fillna(0, inplace=True)
+                elif is_bool_dtype(df[c]):
+                    schema.addColumn(c, Type.BOOLEAN)
+
+
+            table = extract.addTable( 'Extract', schema )
+            if ( table == None ):
+                print('A fatal error occurred while creating the table:\nExiting now\n.')
+                exit( -1 )
+
+    except TableauException as e:
+        print('A fatal error occurred while creating the new extract:\n', e, '\nExiting now.')
+        exit(-1)
+
+
+    try:
+        # Get Schema
+        table = extract.openTable( 'Extract' )
+        schema = table.getTableDefinition()
+
+        for index, row in df.iterrows():
+
+            print(index)
+            print(row)
+            print()
+
+            tabRow = Row( schema )
+
+            colIdx = 0
+
+            for c in df.columns:
+                print(c)
+                print(row[c])
+                print(type(row[c]))
+                if is_string_dtype(df[c]):
+                    if math.isnan(row[c]):
+                        tabRow.setNull(colIdx, row[c])
+                    else:
+                        tabRow.setCharString(colIdx, row[c])
+                elif is_datetime64_dtype(df[c]):
+                    tabRow.setDateTime(colIdx, row[c].year, row[c].month, row[c].day, row[c].hour,  row[c].minute, row[c].second, 0)
+                elif is_integer_dtype(df[c]):
+                    tabRow.setInteger(colIdx, row[c])
+                elif is_float_dtype(df[c]):
+                    tabRow.setDouble(colIdx, row[c])
+                elif is_bool_dtype(df[c]):
+                    tabRow.setBoolean(colIdx, row[c])
+
+                colIdx += 1
+
+            table.insert( tabRow )
+
+    except TableauException as e:
+        print('A fatal error occurred while populating the extract:\n', e, '\nExiting now.')
+        exit( -1 )
+
+    return extract
+
 
 '''
 Creates extract schema
@@ -134,41 +232,25 @@ def populateExtract(
         exit( -1 )
 
 
+def updateDateTypes(df):
+    '''
+    Takes a pandas dateframe and update columns that look like datetimes.
 
-def createTestData():
-    df = pd.read_csv('/Users/ludovicbreger/Data/Tableau/test_df.csv')
-    print(df)
-
-    # Temporary code to see what the columns are like
-    print(df.dtypes)
-
+    :param df: input dataframe
+    :return: None
+    '''
 
     for c in df.columns:
-        print(c)
-        try:
-            df[c] = pd.to_datetime(df[c])
-        except:
-            print('Cannot convert %s', c)
-
-    df['effective_date'] = pd.to_datetime(df['effective_date'])
-
-    print(df.effective_date)
-
-    print(df.dtypes)
-    print(df)
-
-    return df
+        guessDateTypes(df, c)
 
 
-def guessSeriesType(df, c):
+def guessDateTypes(df, c):
     '''
-    Takes a pandas series and comes up with a best guess on its type.
-    This is particularly useful for dates.
+    Takes a pandas dataframe and column c and updates its type to datetime if possible.
 
-    :param s:
-    :return:
+    :param df: input dataframe
+    :return: None
     '''
-
 
     formats = {'%Y-%m-%d': '^[1-2]\d{3}-\d{2}-\d{2}',
                '%m/%d/%Y': '^\d{2}\/\d{2}\/[1-2]\d{3}',
@@ -179,7 +261,7 @@ def guessSeriesType(df, c):
     # as integers will convert just fine
 
     for format, regexp in formats.items():
-        print('Trying to convert column %s to datetime using format %s' % (c, format))
+        print('Trying to convert column "%s" to datetime using format %s' % (c, format))
         if False not in df[c].astype(str).str.contains(regexp).tolist():
         #if False not in df[c].astype(str).str.contains('^[1-2]\d{3}-\d{2}-\d{2}').tolist():
             try:
@@ -227,15 +309,23 @@ if __name__ == "__main__":
 
     print(df.dtypes)
 
-    guessSeriesType(df, 'effective_date')
-    guessSeriesType(df, 'A')
-    guessSeriesType(df, 'test_date')
-    guessSeriesType(df, 'test_date2')
-    guessSeriesType(df, 'test_date3')
+    updateDateTypes(df)
+
+    #guessSeriesType(df, 'effective_date')
+    #guessSeriesType(df, 'A')
+    #guessSeriesType(df, 'test_date')
+    #guessSeriesType(df, 'test_date2')
+    #guessSeriesType(df, 'test_date3')
 
 
     print(df.dtypes)
     print(df)
+
+    #df.fillna(0, inplace=True)
+
+    extract = df2schema(df, '/Users/ludovicbreger/Data/Tableau/test_df1.hyper')
+    fillExtract(extract, df)
+
     exit()
     createTestData()
     retval = main()
