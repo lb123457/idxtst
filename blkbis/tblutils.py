8b666cb121f48b47cd9fd83061489997bcd548c8
ludovicbreger <lb123457@gmail.com>
2018-11-08 22:50:52 -0800

    Fixed conversion problem

--- blkbis/tblutils.py
+++ blkbis/tblutils.py
@@ -11,7 +11,9 @@ import logging
 import pandas as pd
 import numpy as np
 import logging
-
+import argparse
+import getpass
+import logging
 
 import argparse
 import sys
@@ -20,42 +22,13 @@ import textwrap
 from tableausdk import *
 from tableausdk.HyperExtract import *
 
+import tableauserverclient as TSC
+
+from blkbis import dfutils
+
 # Logging setup
 logger = logging.getLogger()
 
-
-
-def __init__():
-    logger.debug('')
-
-
-'''
-Parse Arguments
-'''
-
-def parseArguments():
-    parser = argparse.ArgumentParser( description='A simple demonstration of the Tableau SDK.', formatter_class=argparse.RawTextHelpFormatter )
-    # (NOTE: '-h' and '--help' are defined by default in ArgumentParser
-    parser.add_argument( '-b', '--build', action='store_true', # default=False,
-                         help=textwrap.dedent('''\
-                            If an extract named FILENAME exists in the current directory,
-                            extend it with sample data.
-                            If no Tableau extract named FILENAME exists in the current directory,
-                            create one and populate it with sample data.
-                            (default=%(default)s)
-                            ''' ) )
-    parser.add_argument( '-s', '--spatial', action='store_true', # default=False,
-                         help=textwrap.dedent('''\
-                            Include spatial data when creating a new extract."
-                            If an extract is being extended, this argument is ignored."
-                            (default=%(default)s)
-                            ''' ) )
-    parser.add_argument( '-f', '--filename', action='store', metavar='FILENAME', default='order-py.hyper',
-                         help=textwrap.dedent('''\
-                            FILENAME of the extract to be created or extended.
-                            (default='%(default)s')
-                            ''' ) )
-    return vars( parser.parse_args() )
 
 
 '''
@@ -172,52 +145,34 @@ def df2schema(df, filename):
 
 
 
-def publishExtract(extract):
-    pass
+def publishExtract(args):
+    # SIGN IN
+    tableau_auth = TSC.TableauAuth(args.username, args.password)
+    server = TSC.Server(args.server)
+    server.use_highest_version()
+    with server.auth.sign_in(tableau_auth):
+        # Query projects for use when demonstrating publishing and updating
+        all_projects, pagination_item = server.projects.get()
+        default_project = next((project for project in all_projects if project.is_default()), None)
 
-
-
-def updateDateTypes(df):
-    '''
-    Takes a pandas dateframe and update columns that look like datetimes.
-
-    :param df: input dataframe
-    :return: None
-    '''
-
-    for c in df.columns:
-        guessDateTypes(df, c)
-
-
-def guessDateTypes(df, c):
-    '''
-    Takes a pandas dataframe and column c and updates its type to datetime if possible.
-
-    :param df: input dataframe
-    :return: None
-    '''
-
-    formats = {'%Y-%m-%d': '^[1-2]\d{3}-\d{2}-\d{2}',
-               '%m/%d/%Y': '^\d{2}\/\d{2}\/[1-2]\d{3}',
-               '%Y%m%d': '^[1-2]\d{3}\d{2}\d{2}'
-    }
-
-    # For this format to work we need to make sure that the format matches
-    # as integers will convert just fine
-
-    for format, regexp in formats.items():
-        print('Trying to convert column "%s" to datetime using format %s' % (c, format))
-        if False not in df[c].astype(str).str.contains(regexp).tolist():
-        #if False not in df[c].astype(str).str.contains('^[1-2]\d{3}-\d{2}-\d{2}').tolist():
-            try:
-                df[c] = pd.to_datetime(df[c], format=format)
-            except:
-                print('pd.to_datetime() conversion failed')
+        # Publish datasource if publish flag is set (-publish, -p)
+        if args.publish:
+            if default_project is not None:
+                new_datasource = TSC.DatasourceItem(default_project.id)
+                new_datasource = server.datasources.publish(
+                    new_datasource, args.publish, TSC.Server.PublishMode.Overwrite)
+                print("Datasource published. ID: {}".format(new_datasource.id))
             else:
-                print('Converted column %s to datetime using format %s' % (c, format))
-                return
-        else:
-            print('Format %s because format does not match values' % (format))
+                print("Publish failed. Could not find the default project.")
+
+        # Gets all datasource items
+        all_datasources, pagination_item = server.datasources.get()
+        print("\nThere are {} datasources on site: ".format(pagination_item.total_available))
+        print([datasource.name for datasource in all_datasources])
+
+
+
+
 
 
 #------------------------------------------------------------------------------
@@ -225,53 +180,63 @@ def guessDateTypes(df, c):
 #------------------------------------------------------------------------------
 
 def main():
-    # Parse Arguments
-    options = parseArguments()
 
-    # Extract API Demo
-    if ( options[ 'build' ] ):
-        # Initialize the Tableau Extract API
-        ExtractAPI.initialize()
+    parser = argparse.ArgumentParser( description='A simple demonstration of the Tableau SDK.', formatter_class=argparse.RawTextHelpFormatter )
+    # (NOTE: '-h' and '--help' are defined by default in ArgumentParser
 
-        # Create or Expand the Extract
-        extract = createOrOpenExtract( options[ 'filename' ], options[ 'spatial' ] )
-        populateExtract( extract, options[ 'spatial' ] )
 
-        # Flush the Extract to Disk
-        extract.close()
+    parser.add_argument('--build', '-b', help='path to hyper file to build', default='/Users/ludovicbreger/PycharmProjects/idxtst/test.hyper')
+    parser.add_argument('--publish', '-p', metavar='FILEPATH', help='path to datasource to publish', default=True)
+    parser.add_argument('--download', '-d', metavar='FILEPATH', help='path to save downloaded datasource')
+    parser.add_argument('--server', '-s', help='server address', default='https://public.tableau.com')
+    parser.add_argument('--username', '-u', help='username to sign into server', default='breglud')
+    parser.add_argument('--password', '-x', help='password to sign into server', default='LudoTableau123$')
 
-        # Close the Tableau Extract API
-        ExtractAPI.cleanup()
+    parser.add_argument('--logging-level', '-l', choices=['debug', 'info', 'error'], default='error',
+                        help='desired logging level (set to error by default)')
+
+    args = parser.parse_args()
+
+    if args.build:
+
+        df = pd.read_csv('/Users/ludovicbreger/Data/Tableau/test_df.csv')
+        df['test_date'] = '01/01/2001'
+        df['test_date2'] = '20010101'
+        df['test_date3'] = '2001-01-01 01:01:01.500'
+
+        print(df)
+
+        dfutils.updateDateTypes(df)
+
+        df = df.drop('next_cusip', 1)
+        df = df.head(50)
+        # guessSeriesType(df, 'effective_date')
+        # guessSeriesType(df, 'A')
+        # guessSeriesType(df, 'test_date')
+        # guessSeriesType(df, 'test_date2')
+        # guessSeriesType(df, 'test_date3')
+
+        print(df.dtypes)
+        print(df.shape)
+        print(df)
+
+        # df.fillna(0, inplace=True)
+
+        extract = df2schema(df, args.build)
+
+
+
+    if args.publish | 1 == 1:
+        publishExtract(args)
+
+
 
     return 0
 
 if __name__ == "__main__":
-
-    df = pd.read_csv('/Users/ludovicbreger/Data/Tableau/test_df.csv')
-    df['test_date'] = '01/01/2001'
-    df['test_date2'] = '20010101'
-    df['test_date3'] = '2001-01-01 01:01:01.500'
-
-    print(df.dtypes)
-
-    updateDateTypes(df)
-
-    df = df.drop('next_cusip', 1)
-    df = df.head(50)
-    #guessSeriesType(df, 'effective_date')
-    #guessSeriesType(df, 'A')
-    #guessSeriesType(df, 'test_date')
-    #guessSeriesType(df, 'test_date2')
-    #guessSeriesType(df, 'test_date3')
+    main()
 
 
-    print(df.dtypes)
-    print(df.shape)
-    print(df)
 
-
-    #df.fillna(0, inplace=True)
-
-    extract = df2schema(df, '/Users/ludovicbreger/Data/Tableau/test_df2.hyper')
 
 
